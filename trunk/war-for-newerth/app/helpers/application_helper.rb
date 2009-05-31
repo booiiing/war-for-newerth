9d6f0fab42bfddfe988a8bc02fdfd87369cb7474
xf3l1x <xf3l1x@575981de-4583-11de-aeda-3f4d37dfb5fa>
2009-05-31 19:48:22 +0000

    * Seveal small changes

--- trunk/war-for-newerth/app/helpers/application_helper.rb
+++ trunk/war-for-newerth/app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   # .classname, .classname-top, classname-bottom, .classname-left, .classname-right
   # .classname-top-left, .classname-top-right, .classname-bottom-left, .classname-bottom-right
   #
-  def border style_name='border3', &proc
+  def border style_name, &proc
     concat("<div class='#{style_name}-top'><div class='#{style_name}-left'>" +
            "<div class='#{style_name}-right'><div class='#{style_name}-bottom'>" +
            "<div class='#{style_name}-top-left'><div class='#{style_name}-top-right'>" +
