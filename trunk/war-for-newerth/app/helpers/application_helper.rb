29171a6db4ed4b4f62896f698fc7672e0df35233
xf3l1x <xf3l1x@575981de-4583-11de-aeda-3f4d37dfb5fa>
2009-05-31 11:23:57 +0000

    * Very simple crumbs system implemented

--- trunk/war-for-newerth/app/helpers/application_helper.rb
+++ trunk/war-for-newerth/app/helpers/application_helper.rb
@@ -1,24 +1,39 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  #
+  # Draws a border arround the block given. The border is composed of 8 divs,
+  # not the most elegant way, but browser-safe.
+  # 
+  # You'll have to define styles for the border to take effect. Define following
+  # css rules, where "classname" should ne changed for whatever identifies your
+  # border:
+  # .classname, .classname-top, classname-bottom, .classname-left, .classname-right
+  # .classname-top-left, .classname-top-right, .classname-bottom-left, .classname-bottom-right
+  #
   def border style_name='border3', &proc
-    concat("<div class='#{style_name}-top'>");
-    concat("<div class='#{style_name}-left'>");
-    concat("<div class='#{style_name}-right'>");
-    concat("<div class='#{style_name}-bottom'>");
-    concat("<div class='#{style_name}-top-left'>");
-    concat("<div class='#{style_name}-top-right'>");
-    concat("<div class='#{style_name}-bottom-left'>");
-    concat("<div class='#{style_name}-bottom-right'>");
-    concat("<div class='#{style_name}'>");
+    concat("<div class='#{style_name}-top'><div class='#{style_name}-left'>" +
+           "<div class='#{style_name}-right'><div class='#{style_name}-bottom'>" +
+           "<div class='#{style_name}-top-left'><div class='#{style_name}-top-right'>" +
+           "<div class='#{style_name}-bottom-left'><div class='#{style_name}-bottom-right'>" +
+           "<div class='#{style_name}'>");
     yield proc;
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
-    concat("</div>");
+    concat("</div></div></div></div></div></div></div></div></div>");
   end
+
+  #
+  # Creates the links for the crumbs given a dictionary of name => location and
+  # an optional separator
+  #
+  def crumbs locations={}, separator=' / '
+    rt = link_to '@ Home', :root
+    locations.each_pair do |name, location|
+      rt << separator
+      rt << link_to(name, location)
+    end
+    content_for :crumbs do
+      rt
+    end
+  end
+
 end
