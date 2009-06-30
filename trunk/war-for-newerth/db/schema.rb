f79c9a6b4d19ce56411bffba76c2ed0c04aa84af
xf3l1x <xf3l1x@575981de-4583-11de-aeda-3f4d37dfb5fa>
2009-06-30 19:29:15 +0000

    * Add admin access control on the database

--- trunk/war-for-newerth/db/schema.rb
+++ trunk/war-for-newerth/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090627162800) do
+ActiveRecord::Schema.define(:version => 20090630192501) do
 
   create_table "alliances", :force => true do |t|
     t.string   "name"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20090627162800) do
     t.string   "remember_token",            :limit => 40
     t.datetime "remember_token_expires_at"
     t.integer  "clan_id"
+    t.boolean  "is_admin",                                 :default => false
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
