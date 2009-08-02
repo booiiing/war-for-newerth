1e66ae8a2b4de9f07787ebd62ff49182264aa896
xf3l1x <xf3l1x@575981de-4583-11de-aeda-3f4d37dfb5fa>
2009-08-02 17:43:56 +0000

    * Added strut for the match making system, with all the three options outlined. Needs a lot of style and quality content.

--- war-for-newerth/app/controllers/matches_controller.rb
+++ war-for-newerth/app/controllers/matches_controller.rb
@@ -2,8 +2,22 @@ class MatchesController < ApplicationController
   # GET /match/new
   # GET /match/new.xml
   def new
+    @territory = Territory.find params[:territory] if params[:territory]
     respond_to do |format|
       format.html # new.html.erb
+      format.js do
+        if params[:mode].to_sym == :official
+          render :update do |page|
+            page.loc = false
+            page[:remote_content].replace_html(render :partial => 's2_frame',
+                :locals => {
+                  :url => 'http://www.savage2clans.com/adm1n/clanmatch.php',
+                  :viewport_size => [780, 725],
+                  :frame_size => [780, 2500],
+                  :offset => [0, 660] })
+          end
+        end
+      end
     end
   end
 end
