633d7b6ca91dbc733d70fd92016898f0162cce66
xf3l1x <xf3l1x@575981de-4583-11de-aeda-3f4d37dfb5fa>
2009-08-03 16:02:28 +0000

    * Added territory control to the new match view. Also touched the scroll to be a litle more reliable

--- trunk/war-for-newerth/app/controllers/matches_controller.rb
+++ trunk/war-for-newerth/app/controllers/matches_controller.rb
@@ -8,14 +8,14 @@ class MatchesController < ApplicationController
       format.js do
         if params[:mode].to_sym == :official
           render :update do |page|
-            page.loc = false
+            page << "loc = false;"
             page[:remote_content].replace_html(render :partial => 's2_frame',
                 :locals => {
-                  :url => 'http://www.savage2clans.com/adm1n/clanmatch.php',
-                  :viewport_size => [780, 725],
+                  :url => 'http://www.savage2clans.com/adm1n/clanmatch.php#map[1]',
+                  :viewport_size => [780, 420],
                   :frame_size => [780, 5000],
                   :offset => [0, 0],
-                  :extra_onload => "$('admin-frame').src+='#map[1]';$('frame-container').style.paddingTop='200px';"})
+                  :extra_onload => "setTimeout(function(){$('frame-container').style.paddingTop='200px'}, 500);console.debug('load')"})
           end
         end
       end
